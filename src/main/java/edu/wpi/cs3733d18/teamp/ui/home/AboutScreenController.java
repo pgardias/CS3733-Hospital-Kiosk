6aaa00f979063c6135dd98bb89b0b61732009c26
Przemek Gardias <przemek.gardias@gmail.com>
2018-04-25 13:54:40 -0400

    Copyright information on About screen

--- src/main/java/edu/wpi/cs3733d18/teamp/ui/home/AboutScreenController.java
+++ src/main/java/edu/wpi/cs3733d18/teamp/ui/home/AboutScreenController.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
